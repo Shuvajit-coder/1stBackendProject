9506704a10dbd4161927c4cc678a0eb535bd86fb
Shuvajit_Bera <shuva7086@gmail.com>
2026-01-30 11:09:34 +0530

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit:" 16Kb"}))
-app.use(express.urlencoded({extended: true, limit:"16Kb"}))
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({extended: true, limit:"50mb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
